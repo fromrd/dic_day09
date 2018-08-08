9149697b6322f65fb9c79ff304cccdb2b0d13b3d
fromrd <rirurarura@gmail.com>
2018-08-08 13:44:59 +0000

    init

---
+++ db/migrate/20180805112426_update_users.rb
@@ -0,0 +1,9 @@
+class UpdateUsers < ActiveRecord::Migration[5.1]
+  def change
+    change_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+    end
+  end
+end
